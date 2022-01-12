fd800f8dbd03641ae33035ec15753cb821a7bac3
douglasfs93 <douglas-f-s@hotmail.com>
2022-01-11 21:46:55 -0300

    Pagination layout

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -11,7 +11,7 @@ function Navbar () {
         <a href="https://github.com/douglasfs93">
           <div className='dsmovie-contact-container'>
             <GithubIcon />
-            <p className='dsmovie-contact-link'>/devsuperior</p>
+            <p className='dsmovie-contact-link'>/douglasfs93</p>
           </div>
         </a>
       </div>
